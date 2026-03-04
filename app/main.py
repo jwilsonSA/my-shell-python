cf39e5b1a08d61f460902563f22e49723e7465f1
Jade Wilson <jadewilson@live.co.za>
2026-03-04 16:22:02 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,19 +2,38 @@ import sys
 
 
 def main():
+    # List of built-in commands for our shell
+    builtins = ["echo", "exit", "type"]
+    
     while True:
         sys.stdout.write("$ ")
+        sys.stdout.flush() # Ensures the prompt appears immediately
         
-        # Waitt for user input
+        # Wait for user input
         command = input()
-        if command == "exit":
+        
+        if command == "exit 0":
             break
-        elif command.startswith("echo "):
-            print(f"{command} is a shell builtin")
-        elif command.startswith("exit"):
-            print(f"{command} is a shell builtin")
+        
+        
+        # Split input into the command and its arguments
+        parts = command.split()
+        if not parts:
+            continue
+        
+        cmd = parts[0]
+        
+        if cmd == "type":
+            # Extract what they are trying to chec, e.g., 'type echo'
+            target = parts[1] if len(parts) > 1 else ""
+            if target in builtins:
+                print(f"{target} is a shell builtin")
+            else:
+                print(f"{target} not found")
+        elif cmd in builtins:
+            print(f"{cmd} is a shell builtin")
         else:
-            print(f"{command}: command not found")
+            print(f"{command} is not found")
     
 
 
